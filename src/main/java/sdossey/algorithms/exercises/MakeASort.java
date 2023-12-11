d5310e4ce56b72b17edafcfc4e3e95a66e622542
EthanAmato <ethannamato@gmail.com>
2023-12-11 15:22:29 -0500

    insertion sort (with set- need to try with exchange) and working arraylist that passes tests

--- src/main/java/sdossey/algorithms/exercises/MakeASort.java
+++ src/main/java/sdossey/algorithms/exercises/MakeASort.java
@@ -21,22 +21,36 @@ public class MakeASort {
 //		sort(visualizer.getInstrumentedList());
 		insertionSort(visualizer.getInstrumentedList());
 	}
-/*
- * To sort an array of size N in ascending order iterate over the array and compare 
- * the current element (key) to its predecessor, if the key element is smaller than 
- * its predecessor, compare it to the elements before. Move the greater elements one 
- * position up to make space for the swapped element.
- * */
+
+	/*
+	 * To sort an array of size N in ascending order iterate over the array and
+	 * compare the current element (key) to its predecessor, if the key element is
+	 * smaller than its predecessor, compare it to the elements before. Move the
+	 * greater elements one position up to make space for the swapped element.
+	 */
 	private static void insertionSort(InstrumentedList<Integer> list) {
 
-		for(int i = 0; i < list.size(); i++) {
-			if(i == 0) {
-				
+		Integer currValue;
+		Integer j;
+
+		// guaranteed to have at least 2 elements in list, so this is safe
+		for (int i = 0; i < list.size(); i++) {
+			// Get current item
+			currValue = list.get(i);
+			j = i - 1;
+			// Compare next two:
+
+			// If curr bigger than next, we are not in ascending order
+			while (j >= 0 && list.get(j) > currValue) {
+				// Move each element over by one to make room for next smallest item
+				list.set(j+1, list.get(j));
+				j--;
 			}
-			
-			list.get(i).compareTo(null);
+
+			list.set(j+1, currValue);
+
 		}
-		
+
 	}
 
 	static final void sort(InstrumentedList<Integer> list) {
@@ -51,68 +65,55 @@ public class MakeASort {
 		//
 		// int list.exchange(indexA, indexB);
 		// exchange the element at indexA with the element at indexB
-		
-		// Can't do things like merge sort because we aren't allowed to set indices in subarrays using a setter
+
+		// Can't do things like merge sort because we aren't allowed to set indices in
+		// subarrays using a setter
 
 		// Bubble sort comes to mind when it comes to the exchange method so I'll just
 		// do that
 
 		/*
-		// Loop through entire list:
-		for(int i = 0; i < list.size() - 1; i++) {
-			
-			// Each pass will be met with passes to the end of the list - the amount of passes we've made
-			// so far (since we know that by the end of the first pass the last element will be sorted, there's no need
-			// to revisit and recompare)
-			for(int j = 0; j < list.size() - i - 1; j++) {
-				
-				// If 'left' element turns out to be greater than
-				// 'right' element, we swap
-				if(list.get(j).compareTo(list.get(j + 1)) > 0) {
-					list.exchange(j, j+1);
-				}
-			}
-		}
-		
-		// Avg of O(n^2) time complexity, O(1) Space complexity
-		// Worst Case + Best Case = n^2
-		// Typically, selection sort behaves very badly - ends up being slower than 
-		// bubble sort... Certainly slower than insertion sort despite all of them being O(n^2) time complexity 
-		
-		*/
-		
+		 * // Loop through entire list: for(int i = 0; i < list.size() - 1; i++) {
+		 * 
+		 * // Each pass will be met with passes to the end of the list - the amount of
+		 * passes we've made // so far (since we know that by the end of the first pass
+		 * the last element will be sorted, there's no need // to revisit and recompare)
+		 * for(int j = 0; j < list.size() - i - 1; j++) {
+		 * 
+		 * // If 'left' element turns out to be greater than // 'right' element, we swap
+		 * if(list.get(j).compareTo(list.get(j + 1)) > 0) { list.exchange(j, j+1); } } }
+		 * 
+		 * // Avg of O(n^2) time complexity, O(1) Space complexity // Worst Case + Best
+		 * Case = n^2 // Typically, selection sort behaves very badly - ends up being
+		 * slower than // bubble sort... Certainly slower than insertion sort despite
+		 * all of them being O(n^2) time complexity
+		 * 
+		 */
+
 		// Alternative approach could be selection sort:
 		int currMinIndex;
-		
+
 		// Need to pass through array
-		for(int i = 0; i < list.size(); i++) {
+		for (int i = 0; i < list.size(); i++) {
 			// Keep track of the most recent 'unsorted' index (we'll check the val later)
 			currMinIndex = i;
-			
+
 			// Pass through the rest of the unsorted part of the array
-			for(int j = i; j < list.size(); j++) {
-				
+			for (int j = i; j < list.size(); j++) {
+
 				// If current num in pass is smaller than the min we're tracking,
 				// replace min. By end of pass we will have smallest num in list
-				if(list.get(j) < list.get(currMinIndex)) {
+				if (list.get(j) < list.get(currMinIndex)) {
 					currMinIndex = j;
-				} 
+				}
 			}
-			// Lastly, we will swap the index of the leftmost 'unsorted' index (i) with the index
+			// Lastly, we will swap the index of the leftmost 'unsorted' index (i) with the
+			// index
 			// of the min of the array
 			list.exchange(currMinIndex, i);
 		}
 		// Should also be Time complexity O(n^2) with Space complexity O(1)
-		
-		
-		
-		
-	}
-	
-	
 
-	
-	
-	
-	
+	}
+
 }
--- src/main/java/sdossey/algorithms/exercises/MakeArrayList.java
+++ src/main/java/sdossey/algorithms/exercises/MakeArrayList.java
@@ -36,144 +36,221 @@ import java.util.List;
 //It is okay to add imports if necessary.
 
 public class MakeArrayList<E> extends AbstractList<E> {
-	
-	//You shouldn't need to add any additional fields.
-	int size; //Use this to track the number of items in the list.
-	Object[] storage; //use this as the backing array.
-	//We unfortunately have to use an array of 
-    //objects because of limitations in Java's 
-    //Generic system...
-    //https://docs.oracle.com/javase/tutorial/java/generics/restrictions.html#createArrays
-	
-	//This means we have to cast objects we get out of storage back to (E) type.
-	// IE:  E value = (E)storage[5];
+
+	// You shouldn't need to add any additional fields.
+	int size; // Use this to track the number of items in the list.
+	Object[] storage; // use this as the backing array.
+	// We unfortunately have to use an array of
+	// objects because of limitations in Java's
+	// Generic system...
+	// https://docs.oracle.com/javase/tutorial/java/generics/restrictions.html#createArrays
+
+	// This means we have to cast objects we get out of storage back to (E) type.
+	// IE: E value = (E)storage[5];
 	//
-	//Normally we avoid casts in Java at all costs, but in order to build
-	//and ArrayList this is one rare case where it is unavoidable.
+	// Normally we avoid casts in Java at all costs, but in order to build
+	// and ArrayList this is one rare case where it is unavoidable.
 
-	
-	//Constructor should not be modified.
-    public MakeArrayList()    
-    {
-    	size=0;
-    	storage=new Object[2]; //we'll start with an array of size 2.
-    	//storage.length will always be bigger or the same size as size....
-    	//as it should typically have unfilled space after size.
-    }
+	// Constructor should not be modified.
+	public MakeArrayList() {
+		size = 0;
+		storage = new Object[2]; // we'll start with an array of size 2.
+		// storage.length will always be bigger or the same size as size....
+		// as it should typically have unfilled space after size.
+	}
 
-    //This is used by the test case and shouldn't be modified;
-    public static <E> List<E> factory(E type)
-    {
-        return new MakeArrayList<E>();
-    }
-    
-    //Uncomment the following line only if you get a warning about an 
-    //unchecked cast......feel free to add it to other methods that need it.
-    //
-	//@SuppressWarnings("unchecked")
+	// This is used by the test case and shouldn't be modified;
+	public static <E> List<E> factory(E type) {
+		return new MakeArrayList<E>();
+	}
+
+	// Uncomment the following line only if you get a warning about an
+	// unchecked cast......feel free to add it to other methods that need it.
+	//
+	// @SuppressWarnings("unchecked")
+	@SuppressWarnings("unchecked")
 	@Override
 	public E get(int index) {
 		/* Complete Me! */
 		/* Specification: */
 		/* https://docs.oracle.com/javase/8/docs/api/java/util/List.html#get-int- */
-		/* This will just return an item out of storage in 
-		 * most cases, but you must throw the IndexOutOfBoundsException if a bad
-		 * index is passed in.
+		/*
+		 * This will just return an item out of storage in most cases, but you must
+		 * throw the IndexOutOfBoundsException if a bad index is passed in.
 		 * 
 		 * You will have to cast the Object you grab from storage into an E.
 		 */
-		return null;
+		if (index < 0 || index >= size) {
+		    throw new IndexOutOfBoundsException();
+		}
+		return (E) storage[index];
+
+
 	}
-    
+
 	@Override
-	public E set(int index, E value) {		
+	public E set(int index, E value) {
 		/* Complete Me! */
 		/* Specification: */
 		/* https://docs.oracle.com/javase/8/docs/api/java/util/List.html#set-int-E- */
 		/* This simply replaces an item in storage at index with value */
-		/* YOu need to check and throw IndexOutOfBoundsException in case
-		 * of a bad index just like with get.
+		/*
+		 * YOu need to check and throw IndexOutOfBoundsException in case of a bad index
+		 * just like with get.
 		 */
-		/* Be sure to return the old value previously at the index at the end of
-		 * this method
+		/*
+		 * Be sure to return the old value previously at the index at the end of this
+		 * method
 		 */
-		return null;
+
+		if (index < 0 || index >= size) {
+		    throw new IndexOutOfBoundsException();
+		}
+		E oldValue = (E) storage[index];
+		storage[index] = value;
+		return oldValue;
+
 	}
-	
-	/* This is so trivial I'm giving it to you. 
-	 * No code modification is allowed to this method, but make
-	 * sure you update the size field when you add and remove
+
+	/*
+	 * This is so trivial I'm giving it to you. No code modification is allowed to
+	 * this method, but make sure you update the size field when you add and remove
 	 * items.
 	 */
-    @Override
-    public int size() {        
-        return size;
-    }
-    
-    
-    
-    @Override
-    public void add(int index, E element) {
-    	/* Complete Me! */
-    	/* Specification: */
-    	/* https://docs.oracle.com/javase/8/docs/api/java/util/List.html#add-int-E- */
-    	
-    	/* This inserts item E into the list before index */
-    	/* You will proceed as follows:
-    	 *
-    	 * 1. Check if the index is in range. NOTE that it the legal range is slightly
-    	 *    different than for get() and set() because it is legal specify the 
-    	 *    slot one past the end of the list in order to insert at the end of list.
-    	 *    Throw an IndexOutOfBoundsException if the index is out of range.
-    	 *    
-    	 * 2. Ensure you have room in storage for the new element by comparing
-    	 *    size and storage.length.  If you don't have enough space.....
-    	 *    
-    	 *       a) Allocate a new array of Objects that is twice the size of 
-    	 *          storage.length.
-    	 *       b) copy items 0-size into the new array of Objects.
-    	 *       c) set storage = newStorage; 
-    	 *    These steps will double the space available. We could choose 
-    	 *    to not waste space like this, but we'd end up copying so much
-    	 *    we'd harm the efficiency of the implementation.
-    	 * 
-    	 * 3. Shift all items located at or past index right 1 spot in the array.
-    	 *    When you shift an item right, you need to not overwrite the 
-    	 *    next item before it is shifted right, so you will have to
-    	 *    proceed back to front for the shift.
-    	 *     
-    	 * 4. set the item at index to element.
-    	 * 
-    	 * Make sure you update size by 1 at some point.
-    	 */
-    }
- 
-    @Override
-    public E remove(int index) {
-    	/* Complete Me! */
-    	/* Specification: */
-    	/* https://docs.oracle.com/javase/8/docs/api/java/util/List.html#remove-int- */
-    	
-    	/* This removes item E in the list at index */
-    	/* You will proceed as follows:
-    	 *
-    	 * 1. Check if the index is in range. Throw an
-    	 *    IndexOutOfBoundsException if the index is out of range.
-    	 * 
-    	 * 2. Grab and store the old item at index.
-    	 * 
-    	 * 3. Shift all items past index left 1 spot in the array, replacing
-    	 *    the item at index.
-    	 *    
-    	 * 4. Return the stored old item at index.
-    	 * 
-    	 * Make sure you update size by -1 at some point.
-    	 * 
-    	 * BONUS--as implemented this can be very wasteful because as the ArrayList
-    	 *   shrinks we never shrink storage.  Add code that if 
-    	 *   the storage.length becomes 4x "size" to reallocate and copy
-    	 *   storage to a new storage half as big. 
-    	 */
-    	return null;
-    }
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public void add(int index, E element) {
+//    	System.out.println(index);
+		/* Complete Me! */
+		/* Specification: */
+		/* https://docs.oracle.com/javase/8/docs/api/java/util/List.html#add-int-E- */
+
+		/* This inserts item E into the list before index */
+		/*
+		 * You will proceed as follows:
+		 *
+		 * 1. Check if the index is in range. NOTE that it the legal range is slightly
+		 * different than for get() and set() because it is legal specify the slot one
+		 * past the end of the list in order to insert at the end of list. Throw an
+		 * IndexOutOfBoundsException if the index is out of range.
+		 * 
+		 * 2. Ensure you have room in storage for the new element by comparing size and
+		 * storage.length. If you don't have enough space.....
+		 * 
+		 * a) Allocate a new array of Objects that is twice the size of storage.length.
+		 * b) copy items 0-size into the new array of Objects. c) set storage =
+		 * newStorage; These steps will double the space available. We could choose to
+		 * not waste space like this, but we'd end up copying so much we'd harm the
+		 * efficiency of the implementation.
+		 * 
+		 * 3. Shift all items located at or past index right 1 spot in the array. When
+		 * you shift an item right, you need to not overwrite the next item before it is
+		 * shifted right, so you will have to proceed back to front for the shift.
+		 * 
+		 * 4. set the item at index to element.
+		 * 
+		 * Make sure you update size by 1 at some point.
+		 */
+
+		try {
+			// Check to see if passed in index is the size of the array or less, otherwise
+			// invalid
+			if (index < 0 || index > size)
+				throw new IndexOutOfBoundsException();
+//
+//    		System.out.println("index: " +index);
+//			System.out.println("size: " + size);
+//
+//			System.out.println("storage length: " + storage.length);
+
+			// Check for room in storage:
+			if (size == storage.length - 1) {
+				// Copy over items
+				Object[] newStorage = new Object[storage.length * 2];
+
+				for (int i = 0; i <= size - 1; i++) {
+					newStorage[i] = storage[i];
+				}
+
+				this.storage = newStorage;
+			}
+
+			// Add item by first:
+
+			// Moving items from index over by one:
+			for (int i = size; i > index; i--) {
+			    storage[i] = storage[i - 1];
+			}
+			storage[index] = element;
+			size++;
+		} catch (IndexOutOfBoundsException e) {
+			throw new IndexOutOfBoundsException("Index out of range...");
+		}
+
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public E remove(int index) {
+		/* Complete Me! */
+		/* Specification: */
+		/* https://docs.oracle.com/javase/8/docs/api/java/util/List.html#remove-int- */
+
+		/* This removes item E in the list at index */
+		/*
+		 * You will proceed as follows:
+		 *
+		 * 1. Check if the index is in range. Throw an IndexOutOfBoundsException if the
+		 * index is out of range.
+		 * 
+		 * 2. Grab and store the old item at index.
+		 * 
+		 * 3. Shift all items past index left 1 spot in the array, replacing the item at
+		 * index.
+		 * 
+		 * 4. Return the stored old item at index.
+		 * 
+		 * Make sure you update size by -1 at some point.
+		 * 
+		 * BONUS--as implemented this can be very wasteful because as the ArrayList
+		 * shrinks we never shrink storage. Add code that if the storage.length becomes
+		 * 4x "size" to reallocate and copy storage to a new storage half as big.
+		 */
+		try {
+			if (index < 0 || index > size)
+				throw new IndexOutOfBoundsException();
+
+			E oldValue = (E) storage[index];
+			for (int i = index; i < size - 1; i++) {
+			    storage[i] = storage[i + 1];
+			}
+			storage[size - 1] = null;
+			size--;
+			
+			if (storage.length > size * 4) {
+			    Object[] newStorage = new Object[storage.length / 2];
+			    System.arraycopy(storage, 0, newStorage, 0, size);
+			    storage = newStorage;
+			}
+
+			return oldValue;
+
+		} catch (IndexOutOfBoundsException e) {
+			throw new IndexOutOfBoundsException("Index out of range...");
+		}
+
+	}
+//	public static void main(String[] args) {
+//		MakeArrayList<Integer> test = new MakeArrayList<>();
+//		
+//		test.add(16);
+//
+//		test.add(161);
+//		System.out.println(test);
+//	}
+
 }
